86da6c1fe3fb64af5722d7cd0b8193c2c4d498cb
664742 <83961134+664742@users.noreply.github.com>
2023-02-19 09:25:27 -0500

    Update filter working version.ino
    arduino ino file for sensors

--- arduino/filter working version/filter working version.ino
+++ arduino/filter working version/filter working version.ino
@@ -1,5 +1,7 @@
 #include <hcsr04.h>
 #include <SharpDistSensor.h>
+#include "string.h"
+using namespace std; 
 
 //Pins for IR Sensors
 const byte sharpPin1 = A0;
@@ -49,7 +51,9 @@ void loop() {
 
   double r_us = (double)us2.distanceInMillimeters()/1000;
   double r_ir = (double)sharp2.getDist()/100;
-  
+
+  int l_vol;
+  int r_vol;
   
   if(counter < 10){
     ir[counter] = l_ir;
@@ -63,30 +67,84 @@ void loop() {
     counter = 0;
     if (l_us > 1.5 && l_us < 2.2) {
       distance1 = average;
+      l_vol = (distance1 * -25) + 125;
     }
     else if(l_us < 1.5){
       distance1 = l_us;
+      l_vol = 100;
     }
     else if(l_ir > 2.2){
       distance1 = l_ir;
+      l_vol = (distance1 * -25) + 125;
     }
 
     if (r_us > 1.5 && r_us < 2.2) {
       distance2 = (r_us + r_ir)/2;
+      r_vol = (distance2 * -25) + 125;
+
     }
     else if(r_us < 1.5){
       distance2 = r_us;
+      r_vol = 100;
+
     }
     else if(r_ir > 2.2){
       distance2 = r_ir;
+      r_vol = (distance2   * -25) + 125;
+
     }
 
-    Serial.print("US1: ");
-    Serial.print(l_us);
-    Serial.print(" IR1: ");
-    Serial.print(l_ir);
-    Serial.print(" D: ");
-    Serial.println(distance1);
+    String leftvol;
+    String rightvol;
+/*
+    if(l_vol < 100 & l_vol > 10){
+      leftvol = '0' + (String)((int)l_vol);
+    }
+    else if(l_vol < 10 && l_vol > 0){
+      leftvol = '0' + '0' + (String)((int)l_vol);
+    }
+    else{
+      leftvol = '000';
+    }
+    if(r_vol < 100 & r_vol > 10){
+      rightvol = '0' + (String)((int)r_vol);
+    }
+    else if(r_vol < 10 && r_vol > 0){
+      rightvol = '0' +'0' + (String)((int)r_vol);
+    }
+    else{
+      rightvol = '000';
+    }
+*/
+    // String l0 = "";
+    // String r0 = "";
+    // int l = l_vol;
+    // int r = r_vol;
+
+    // for (int i = 0; i < 1; i++)
+    // {
+    //     if (l / 10 < 10)
+    //     {
+    //         l0 += '0';
+    //         l/=10;
+    //     }
+    //     if (r / 10 < 10)
+    //     {
+    //         r0 += '0';
+    //         r/=10;
+    //     }
+    // }
+
+    String send = String(l_vol) + " " +String(r_vol);
+    
+    // Serial.print("US1: ");
+    Serial.println(send);
+    Serial.write(13);
+    Serial.write(10);
+    // Serial.print(" IR1: ");
+    // Serial.print(l_ir);
+    // Serial.print(" D: ");
+    // Serial.println(distance1);
   }
   
 
